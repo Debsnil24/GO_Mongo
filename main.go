d868bf692229044b054fd10e1a39b773e0c2d7ba
Debsnil24 <debsnilsamudra@gmail.com>
2024-12-28 03:11:53 +0530

    Created necessary functions & connections **Summary** - Created the routes for the http server and added code to start the local server - Created a function to connect to the local MongoDB session - Created a new User struct in the models package

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"net/http"
+	"github.com/Debsnil24/GO_Mongo.git/controllers"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
-	"net/http"
 )
 
 func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
+}
 
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27107")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
